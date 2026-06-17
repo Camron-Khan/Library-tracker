f317b093787d7c18038ce590205dc23e572e282e
Camron Khan <Camron.Khan@mail.citytech.cuny.edu>
2026-06-17 00:32:04 -0400

    Sub array finished

--- src/arraylist.cpp
+++ src/arraylist.cpp
@@ -1,7 +1,7 @@
 #include <cstddef>
-#include "arraylist.hpp"
 #include <stdlib.h>
 #include <stdio.h>
+#include "arraylist.hpp"
 void initArray( sArray *arr,size_t initial_capacity){
    arr->eSize = 0;
    arr->contSize = initial_capacity;
@@ -13,14 +13,32 @@ void initArray( sArray *arr,size_t initial_capacity){
 
 }
 
-void addArray(sArray *arr, char **element){
+void addArray(sArray *arr, char *element){
     if (arr->eSize == arr->contSize){
     	arr->eSize = 2;
      	void *temp = realloc(arr->data, arr->contSize * sizeof(char));
       	if (temp == NULL){
-       		return ;
+       		printf("Reallcation failed");
        }
     }
+    arr->data[arr->eSize] = (char *)malloc((strlen(element) + 1) * sizeof(char));
+    if (arr->data[arr->eSize] == NULL){
+    	printf("String allocation failed");
+}
+    strcpy(arr->data[arr->eSize], element);
+}
 
+void subArray(sArray *arr, size_t index){
+	if (index >= arr->eSize){
+		printf("Out of bounds");
+}
+	free(arr->data[index]);
+	if (index != arr->eSize){
+		arr->data[index] = arr->data[arr->eSize -1];
+}
+	arr->eSize --;
+}
+
+void srArray(sArray *arr, size_t index){
 
 }
