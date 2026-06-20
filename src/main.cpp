d7780d3099e1417aa080213f4942ca471ee49f87
Camron003 <86741853+Camron003@users.noreply.github.com>
2026-06-19 21:19:46 -0400

    The main function is being worked on

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include "arraylist.hpp"
+#include <cstring>
 
 void printMenu(){
 	printf(" 1 for adding list");
 	printf("/n 2 for subtracting list");
 	printf("/n 3 for seacjing list");
 }
+// Maybe I'll do a string input function to create a string without size
+//void stringInp(){}
 
 int main(){
 	int menuOpt = 0;
+    sArray user;
 	while(true){
 		printMenu();
 		scanf("%d", menuOpt);
 		switch(menuOpt)
 		case 1:
-			//add
+          char addName[50];
+          printf("What do you want to add to the list?");
+          scanf("%s",addName);
+          addArray(&user, addName);
 			break;
 		case 2:
+          char subName[50];
+          printf("What do you want to delete?");
+          scanf("%s", subName);
+          subArray(&user, subName);
 			//subtracting
 			break;
 		case 3:
+          char searchItem[50];
+          printf("What do you want to look for?");
+          scanf("%s", searchItem);
 			//search
 			break;
 		default:
