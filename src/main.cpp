aac31e4763e40dcaae54357f13b53e74c2a526db
Camron003 <86741853+Camron003@users.noreply.github.com>
2026-06-25 23:58:36 -0400

    main function has the fget instead of scanf

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "arraylist.hpp"
 #include <cstring>
+#include <cstdio>
 
 void printMenu(){
 	printf(" 1 for adding list");
@@ -10,33 +11,34 @@ void printMenu(){
 //void stringInp(){}
 
 int main(){
+    char nameBuff[50];
 	int menuOpt = 0;
     sArray user;
 	while(true){
 		printMenu();
-		scanf("%d", menuOpt);
-		switch(menuOpt)
+      scanf("%d", menuOpt);
+		switch(menuOpt){
 		case 1:
-          char addName[50];
+          // This function is to add something to the list
           printf("What do you want to add to the list?");
-          scanf("%s",addName);
-          addArray(&user, addName);
+          fgets(nameBuff,sizeof(nameBuff),stdin);
 			break;
 		case 2:
+          // This function is the remove something from the list
           char subName[50];
           printf("What do you want to delete?");
-          scanf("%s", subName);
-          subArray(&user, subName);
+          fgets(nameBuff,sizeof(nameBuff),stdin);
+          subArray(&user, nameBuff);
 			//subtracting
 			break;
 		case 3:
+           // This function is the search for something in the list
           char searchItem[50];
           printf("What do you want to look for?");
-          scanf("%s", searchItem);
-			//search
+          fgets(nameBuff,sizeof(nameBuff),stdin);
 			break;
 		default:
 			printf("incorrect option");
-}
-
+    }
+  }
 }
