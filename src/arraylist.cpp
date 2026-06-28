82db62e17f332305fa9a629fe0ce306e6df4fd14
Camron003 <86741853+Camron003@users.noreply.github.com>
2026-06-27 23:22:21 -0400

    have to fix dynamic arrays

--- src/arraylist.cpp
+++ src/arraylist.cpp
@@ -3,10 +3,9 @@
 #include <string.h>
 #include "arraylist.hpp"
 void initArray( sArray *arr,size_t initial_capacity){
-   arr->eSize = 0;
    arr->contSize = initial_capacity;
    arr->data = (char ** )(arr->contSize * sizeof(char *));
-
+   arr->elementSize = NULL;
 
    if (arr == NULL){
        printf("Memory allocation failed");
@@ -14,19 +13,26 @@ void initArray( sArray *arr,size_t initial_capacity){
 
 }
 
-void addArray(sArray *arr, char *element){
-    if (arr->eSize == arr->contSize){
-    	arr->eSize = 2;
-     	void *temp = realloc(arr->data, arr->contSize * sizeof(char));
-      	if (temp == NULL){
-       		printf("Reallcation failed");
-       }
-    }
-    arr->data[arr->eSize] = (char *)malloc((strlen(element) + 1) * sizeof(char));
-    if (arr->data[arr->eSize] == NULL){
-    	printf("String allocation failed");
-    }
-    std::strcpy(arr->data[arr->eSize], element);
+void addArray(sArray *arr, char *target){
+  for (int i = 0; arr->elementSize[i] > arr->contSize; i++){
+    if (arr)
+  }
+
+
+
+// This is the old reference for the add Array function
+  //if (arr->eSize == arr->contSize){
+    //arr->eSize = 2;
+    //	void *temp = realloc(arr->data, arr->contSize * sizeof(char));
+    // 	if (temp == NULL){
+    //  		printf("Reallcation failed");
+      // }
+    //}
+    //arr->data[arr->eSize] = (char *)malloc((strlen(element) + 1) * sizeof(char));
+    //if (arr->data[arr->eSize] == NULL){
+    	//printf("String allocation failed");
+    //}
+    //std::strcpy(arr->data[arr->eSize], element);
 }
 
 void subArray(sArray *arr, char *target, size_t size){
@@ -38,7 +44,7 @@ void subArray(sArray *arr, char *target, size_t size){
   }
 }
 
-void srArray(sArray *arr, size_t size, char *target){
+void srArray(sArray *arr, char *target, size_t size){
 	for (int i = 0; i < size; i++){
 		if(std::strcmp(arr->data[i], target) == 0){
 			printf("String exists");
