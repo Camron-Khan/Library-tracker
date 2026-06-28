6df51c742bdb5b5f8fe89cc951d64db21982a251
Camron003 <86741853+Camron003@users.noreply.github.com>
2026-06-27 23:20:48 -0400

    working to fix faulty dynamic array

--- src/arraylist.hpp
+++ src/arraylist.hpp
@@ -3,14 +3,13 @@
 #include <stdio.h>
 typedef struct{
     char **data;
-    size_t eSize;
     size_t contSize;
 
 }sArray;
 
 void initArray( sArray *arr,size_t initial_capacity);     // This will initilize the array
-void addArray(sArray *arr, char **b);  // This will add something to the array
-void subArray(sArray *arr, char *target, size_t size);  // this will remove an element from an array
-void srArray(sArray *arr, size_t size, char *target);   // this will search though the array for an element
+void addArray(sArray *arr, char *target);  // This will add something to the array
+void subArray(sArray *arr, char *target);  // this will remove an element from an array
+void srArray(sArray *arr, char *target);   // this will search though the array for an element
 
 #endif
